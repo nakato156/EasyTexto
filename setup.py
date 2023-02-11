720e10ce3054f4e7ae3036c4412f332328851e1d
nakato156 <christianvelasces@gmail.com>
2023-02-11 14:14:15 -0500

    change doc

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='TextoVEasy',
-    version='0.1',
+    version='0.1.1',
     license='MIT',
     description='Paquete para manejar archivos de texto de forma sencilla',
     long_description_content_type="text/markdown",
